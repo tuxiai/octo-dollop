c58dc45318e11bd3b3decdad3c235b3cf010d315
tuxiai <89543767+tuxiai@users.noreply.github.com>
2023-05-01 20:11:10 +0000

    help list written out

--- commands.py
+++ commands.py
@@ -1,9 +1,30 @@
 #help
+def help():
+	print('''Commands:
+"h" or "help" for a list of commands
+"i" or "inventory" to see your items
+"t" or "talk" to talk to characters
+"c" or "check" to check a room for items
+"g" or "grab" to take an item
+"u" or "use" to use an item
+"s" or "save" to save
+"l" or "load" to load
+
+Movement:
+"door [number]" to enter that door
+"leave" to leave the room you're in
+	(leaving a room will take you back to the cafe)
+
+P.S. Press enter to advance text!
+''')
+
 #movement
+	#door
+	#leave
 #inventory
 #talk
+#check [for items]
 #grab
 #use
 #save
 #load
-#check [for items]
