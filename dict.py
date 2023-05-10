3e8e1ac815cbe36db09789ac13b9bf2427a44641
tuxiai <89543767+tuxiai@users.noreply.github.com>
2023-05-10 23:48:11 +0000

    item desc added

--- dict.py
+++ dict.py
@@ -5,7 +5,9 @@ cdict = {
 	#playground
 	"pocket sand":"That sandbox is looking awfully sandy.",
 	#stargazers
-	"goose":"That goose is honking up a storm."
+	"goose":"That goose is honking up a storm.",
+	#dungeon
+	"moss":"There's some moss in the corner."
 	}
 
 
@@ -18,17 +20,23 @@ gdict = {
 	"money":"Some crumpled up bills.",
 	#stargazers
 	"goose":f"You grabbed the goose. It starts honking aggressively, but otherwise does nothing.\nApron guy looks a little awestruck.\n\"Oh, thanks for dealing with that. I didn't know what to do.\"",
-	"soda":"Looks cherry flavored."
+	"soda":"Looks cherry flavored.",
+	#dungeon
+	"moss":"Are you going to eat it?",
+	"sword":"A pretty hefty sword."
 	}
 
 chara = {
 	"boss":"Your boss is doing.. boss things.",
 	"scrib":"There's a child in purple playing in the sandbox.",
-	"oli":"You see a guy in a pink apron at the counter."
+	"oli":"You see a guy in a pink apron at the counter.",
+	"skel":"There's and armored skeleton. Oh. It's alive."
+	"shopkeep":"The shopkeeper looks so tired."
 	}
 
 cafechara = {
 	"boss":"Your boss is doing.. boss things.",
 	"scrib":"You see the kid from the playground eating at one of the tables near a window.",
 	"oli":"The guy from the other cafe is here too. He's talking to your boss."
+	"skel":"The skeleton from the dungeon seems to be drinking... milk?"
 	}
